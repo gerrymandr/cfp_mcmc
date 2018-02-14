b17d382ed46d7e2d59b2198f54bb0d978808cf11
Michael Sarahan <msarahan@gmail.com>
2018-02-13 19:32:54 -0600

    tweak run script for running in multiple folders

--- scripts/get_results.sh
+++ scripts/get_results.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 set -e
+target_time=0.01
 
 make
-rm histograms/*
+
+data_root="$( cd "$(dirname "$0")" ; pwd -P )"/..
+
+PLANS=( "CurrentRep.txt" "NewRep.txt" )
 INPUTS=( "cong12.csv" "cong16.csv" "pres08.csv" "pres12.csv" "pres16.csv" "sen10.csv" "sen16.csv")
 RESULTS=( "Feb_8_Wes_units.csv" "Feb_11_Wes_units.csv" )
-for input in "${INPUTS[@]}"; do
-    for result in "${RESULTS[@]}"; do
-        echo $input $result
-        ./chain -f CurrentRep.txt -n 21 -d 22 -1 68 -M -p 0.1 --histogram --filename_wes_units bill_plans/$result --filename_election_results $input | grep median_mean | tail -1 | awk '{ print $NF }'
-        cp median_mean_hist.txt histograms/histogram-$result-$input
+for plan in "${PLANS[@]}"; do
+    for input in "${INPUTS[@]}"; do
+        for result in "${RESULTS[@]}"; do
+            [[ "$plan" == "CurrentRep.txt" ]] && l1_value=158 || l1_value=69
+            echo $plan $input $result
+            folder=$plan_$input_$result
+            mkdir -p $data_root/$folder
+            pushd $data_root/$folder
+            ../chain -f $data_root/$plan -n 9999 -d 22 -1 $l1_value -M -p 0.1 --histogram --filename_wes_units $data_root/bill_plans/$result --filename_election_results $data_root/$input --target_time $target_time > out_err.log || echo "errored: $folder"
+            popd
+        done
     done
 done
